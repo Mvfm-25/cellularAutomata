4689766a98a5ee4e62cb6cebbf84b1850be8f525
Mvfm <mvfm.filho@hotmail.com>
2026-01-10 19:09:19 -0300

    Correção na data de modificação.

--- cellularAutomata.py
+++ cellularAutomata.py
@@ -1,7 +1,7 @@
 # Brincando com geração de cavernas usando cellular automata.
 # [mvfm]
 #
-# Criação : 09/01/2026  ||  Última modificação : 09/01/2026
+# Criação : 09/01/2026  ||  Última modificação : 10/01/2026
 
 import numpy as np
 import random
